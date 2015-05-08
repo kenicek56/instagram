6f1e1a4963c9e70d1bad294a539b3812f0c0604c
kenice washington <kenicek56@gmail.com>
2015-05-08 12:29:28 -0700

    finished video 4

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //make constants usinf define
-define('client_id',	'fa48296f5aa047f48fb9b9192bac9238'); 
-define('client_secret', 'b2974fae436c47a890b6a090b1777897'); 
+define('clientID',	'fa48296f5aa047f48fb9b9192bac9238'); 
+define('clientSecret', 'b2974fae436c47a890b6a090b1777897'); 
 define('redirectURI', 'http://localhost/Kenicesta/index.php'); 
 define('ImageDirectory','pics/'); 
  ?>
