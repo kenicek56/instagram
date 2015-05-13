8ecf1b6b509932ac5a5cd8655e8ee35b7a240369
kenice washington <kenicek56@gmail.com>
2015-05-13 11:52:04 -0700

    i mean i finished video 5

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ define('clientSecret', 'b2974fae436c47a890b6a090b1777897');
 define('redirectURI', 'http://localhost/Kenicesta/index.php'); 
 define('ImageDirectory','pics/'); 
  
-if isset(($_GET['code'])) {
+if (isset($_GET['code'])) {
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
 	$access_token_settings = array('client_id' => clientID,
